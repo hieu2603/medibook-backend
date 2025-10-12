5da1acdc76fbc441c2c55610ba2f288668e409b2
hieu2603 <nguyenvanhieu2603@gmail.com>
2025-10-12 19:48:35 +0700

    Initial commit - Medibook backend

---
+++ clinic-service/src/test/java/com/sgu/clinic_service/ClinicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.clinic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ email-service/src/test/java/com/sgu/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
